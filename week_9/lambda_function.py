bad5bb87d1c1bce1f7a2770f308eded62bbef305
Emma Carballal <carballal.emma@gmail.com>
2022-11-28 23:19:40 +0100

    Fix code to build and run docker container

--- week_9/lambda_function.py
+++ week_9/lambda_function.py
@@ -1,18 +1,17 @@
 import numpy as np
-import tensorflow.lite as tflite
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from io import BytesIO
 from urllib import request
-
 from PIL import Image
+import os
+import tflite_runtime.interpreter as tflite
 
-interpreter = tflite.Interpreter(model_path='dino_dragon_10_0.899.tflite')
+MODEL_NAME = os.getenv('MODEL_NAME', 'dino-vs-dragon-v2.tflite')
+
+interpreter = tflite.Interpreter(model_path=MODEL_NAME)
 interpreter.allocate_tensors()
 
 input_index = interpreter.get_input_details()[0]['index']
 output_index = interpreter.get_output_details()[0]['index']
-
-
 
 def download_image(url):
     with request.urlopen(url) as resp:
@@ -21,28 +20,27 @@ def download_image(url):
     img = Image.open(stream)
     return img
 
-
 def prepare_image(img, target_size):
     if img.mode != 'RGB':
         img = img.convert('RGB')
     img = img.resize(target_size, Image.NEAREST)
     return img
 
-url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/1280px-Smaug_par_David_Demaret.jpg"
+# url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/1280px-Smaug_par_David_Demaret.jpg"
 
 def predict(url):
     img = download_image(url)
     resized_img = prepare_image(img, (150, 150))
     x = np.array(resized_img, dtype='float32')
     X = np.array([x])
-    generator = ImageDataGenerator(rescale=1./255)
-    X = generator.flow(X, batch_size=1)[0]
+    X /= 255.0
     interpreter.set_tensor(input_index, X)
     interpreter.invoke()
     pred = interpreter.get_tensor(output_index)
-    return pred[0]
+    return pred[0].tolist()
 
 def lambda_handler(event, context):
     url = event['url']
-    result = predict(url)
+    pred = predict(url)
+    result = {'prediction': pred}
     return result
